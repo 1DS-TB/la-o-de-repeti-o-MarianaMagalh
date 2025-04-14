98255f70d06ab90e221ad0c8a03d930f16f976d0
Mariana Macena <167040224+MarianaMagalh@users.noreply.github.com>
2025-04-14 08:31:25 -0300

    add exer 7 ao 9 (precisa de revisão)

---
+++ .github/exercicio7.py
@@ -0,0 +1,6 @@
+n = int(input("Insira um número: "))
+m = " "
+
+for i in range(1, n + 1):
+    m = m + "*"
+    print(m)
---
+++ .github/exercicio8.py
@@ -0,0 +1,6 @@
+n = int(input("Insira os números: "))
+contador = 1
+while contador <= n:
+    contador += 1
+    s = 1 + (1/n)
+print(f"Serie harmonica: {s:.2f}")
